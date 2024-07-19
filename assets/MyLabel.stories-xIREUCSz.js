b85821cb509ad696efd96418ab785e4f2c259dbc
Felipe-Navas <felipenavas.itec@gmail.com>
2024-07-19 23:27:50 +0000

    Deploying to gh-pages from @ Felipe-Navas/my-storybook-vite@fd15b62b13fe1664aa03c89d3c5fd760ebcf1ac7 🚀

--- assets/MyLabel.stories-xIREUCSz.js
+++
@@ -1,5 +0,0 @@
-import{j as o}from"./jsx-runtime-QvZ8i92b.js";import"./index-uubelm5h.js";const l=({label:t,size:n="normal"})=>o.jsx("span",{className:`${n}`,children:t});l.__docgenInfo={description:"",methods:[],displayName:"MyLabel",props:{label:{required:!0,tsType:{name:"string"},description:""},size:{required:!1,tsType:{name:"union",raw:"'normal' | 'h1' | 'h2' | 'h3'",elements:[{name:"literal",value:"'normal'"},{name:"literal",value:"'h1'"},{name:"literal",value:"'h2'"},{name:"literal",value:"'h3'"}]},description:"",defaultValue:{value:"'normal'",computed:!1}}}};const c={title:"UI/Labels/MyLabel",component:l,tags:["autodocs"],parameters:{layout:"centered"}},e={args:{label:"Basic Label"}};var a,r,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    label: 'Basic Label'
-  }
-}`,...(s=(r=e.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const p=["Basic"];export{e as Basic,p as __namedExportsOrder,c as default};
