fd15b62b13fe1664aa03c89d3c5fd760ebcf1ac7
Felipe Navas <felipenavas.itec@gmail.com>
2024-07-19 20:26:47 -0300

    Added some props and Stories

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -1,10 +1,42 @@
 import './MyLabel.css'
 
 interface Props {
+  /**
+   * Text to be displayed
+   */
   label: string
+
+  /**
+   * Size of the label
+   */
   size?: 'normal' | 'h1' | 'h2' | 'h3'
+
+  /**
+   * Whether the label should be all caps
+   */
+  allCaps?: boolean
+
+  /**
+   * Color of the label
+   */
+  color?: 'text-primary' | 'text-secondary' | 'text-tertiary'
+
+  /**
+   * Font color of the label
+   */
+  fontColor?: string
 }
 
-export const MyLabel = ({ label, size = 'normal' }: Props) => {
-  return <span className={`${size}`}>{label}</span>
+export const MyLabel = ({
+  label,
+  size = 'normal',
+  allCaps = false,
+  color = 'text-primary',
+  fontColor,
+}: Props) => {
+  return (
+    <span style={{ color: fontColor }} className={`${size} ${color} label`}>
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  )
 }
--- src/stories/MyLabel.stories.ts
+++ src/stories/MyLabel.stories.ts
@@ -8,6 +8,11 @@ const meta = {
   parameters: {
     layout: 'centered',
   },
+  argTypes: {
+    size: {
+      control: 'inline-radio',
+    },
+  },
 } satisfies Meta<typeof MyLabel>
 
 export default meta
@@ -17,5 +22,26 @@ type Story = StoryObj<typeof meta>
 export const Basic: Story = {
   args: {
     label: 'Basic Label',
+  },
+}
+
+export const AllCaps: Story = {
+  args: {
+    label: 'All Caps Label',
+    allCaps: true,
+  },
+}
+
+export const Secondary: Story = {
+  args: {
+    label: 'Secondary Label',
+    color: 'text-secondary',
+  },
+}
+
+export const CustomColor: Story = {
+  args: {
+    label: 'Custom Color Label',
+    fontColor: '#146b86',
   },
 }
